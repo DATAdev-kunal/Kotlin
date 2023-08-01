204428fa32822ed695022d69e2ca3e27d2568435
DATAdev-kunal <kunal.datadev.os@gmail.com>
2023-08-02 04:07:54 +0530

    getting year

--- 26.Date_Time_API/ClassDate.java
+++ 26.Date_Time_API/ClassDate.java
@@ -1,5 +1,20 @@
+import java.sql.Date;
+
 public class ClassDate {
     public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+        System.out.println(Long.MAX_VALUE); //max value that a long value can store
+
+
+        Date d=new Date(2023, 8, 1);
+        System.out.println(d.getTime());
+        System.out.println(d.getDate());
+        System.out.println(d.getDay());
+        System.out.println(d.getYear());
         
+        System.out.println(System.currentTimeMillis()/(1000*60*60*24*365));
+
+       System.out.println( d.toLocalDate());
+       
     }
 }
